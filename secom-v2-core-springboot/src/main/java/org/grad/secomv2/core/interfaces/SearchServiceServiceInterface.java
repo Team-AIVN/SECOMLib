4d11f1f774b2f2cde7bd4d0900cd78b6be9a24e2
jakob22r <jakob.svenningsen@outlook.com>
2026-09-01 14:13:12 +0200

    SearchResult must implement envelopesignatureBearer

--- secom-v2-core-springboot/src/main/java/org/grad/secomv2/core/interfaces/SearchServiceServiceInterface.java
+++ secom-v2-core-springboot/src/main/java/org/grad/secomv2/core/interfaces/SearchServiceServiceInterface.java
@@ -81,7 +81,6 @@ public interface SearchServiceServiceInterface extends GenericSecomInterface {
         // Handle according to the exception type
         if(ex instanceof SecomSignatureVerificationException) {
             httpStatus = HttpStatus.UNAUTHORIZED;
-            searchResult.setMessage("Unauthorized");
         } else if(ex instanceof SecomValidationException
                 || ex.getCause() instanceof SecomValidationException
                 || ex instanceof ValidationException
@@ -89,13 +88,10 @@ public interface SearchServiceServiceInterface extends GenericSecomInterface {
                 || ex instanceof HttpClientErrorException.NotFound
                 || ex instanceof JsonParseException) {
             httpStatus = HttpStatus.BAD_REQUEST;
-            searchResult.setMessage("Bad Request");
         } else if(ex instanceof SecomNotFoundException) {
             httpStatus = HttpStatus.NOT_FOUND;
-            searchResult.setMessage("Information not found");
         } else {
             httpStatus = GenericSecomInterface.handleCommonExceptionResponseCode(ex);
-            searchResult.setMessage(httpStatus.getReasonPhrase());
         }
 
         return ResponseEntity
